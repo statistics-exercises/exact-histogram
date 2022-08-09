5d1b792a806da20678579e1c4a409ab1089eef49
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-09 11:03:29 +0100

    Fixed test routine

--- testing/test_main.py
+++ testing/test_main.py
@@ -12,13 +12,13 @@ from AutoFeedback.randomclass import randomvar
 import unittest
 from main import *
 
-x = [0,1,3,4,5,6,0,1,2,3,4,5,6]
+x = [0-0.05,1-0.05,2-0.05,3-0.05,4-0.05,5-0.05,6-0.05,0+0.05,1+0.05,2+0.05,3+0.05,4+0.05,5+0.05,6+0.05]
 var = np.ones(14)*1/6
 var[0] = 0
-for i in range(7) : var[7+i] = scipy.stats.norm.pmf(i,6,0.5)
+for i in range(7) : var[7+i] = scipy.stats.binom.pmf(i,6,0.5)
 line1=line( x, var )
 
-axislabels=["x", "P(x)"]
+axislabels=["x", "P(X=x)"]
 
 class UnitTests(unittest.TestCase) :
     def test_plot(self) :
